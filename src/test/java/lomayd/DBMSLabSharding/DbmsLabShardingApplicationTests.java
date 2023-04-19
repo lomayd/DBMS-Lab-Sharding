61d7f6a050b2619a3c90def3015ecfb8180e51a2
lomayd <jinho10167@gmail.com>
2023-04-19 06:43:26 +0000

    feat: Project Initialize

---
+++ src/test/java/lomayd/DBMSLabSharding/DbmsLabShardingApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabSharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsLabShardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
