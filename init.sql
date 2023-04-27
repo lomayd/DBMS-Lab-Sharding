b9887cbbc93220a4621246b958b68a664b378871
lomayd <jinho10167@gmail.com>
2023-04-28 06:14:35 +0900

    feat: MySQL Initialize

---
+++ init.sql
@@ -0,0 +1,21 @@
+USE LAB;
+
+CREATE TABLE LAB.User_0
+(
+    id varchar(255),
+    password varchar(255),
+    name varchar(255),
+    nickname varchar(255),
+    email varchar(255),
+    age int
+);
+
+CREATE TABLE LAB.User_1
+(
+    id varchar(255),
+    password varchar(255),
+    name varchar(255),
+    nickname varchar(255),
+    email varchar(255),
+    age int
+);
